b13d740be045ac118809dffc582deea154c2a849
anuraggarg3 <anurag.3garg@gmail.com>
2024-07-05 23:52:42 +0530

    first commit

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+  Text,
+  View
+} from 'react-native';
+const App = () => {
+  return (
+    <View>
+      <Text>dafs text</Text>
+    </View>
+  );
+}
+
+export default App;
